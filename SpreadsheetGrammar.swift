4aba1a4d6f762d4421a91b8a33b5920dd8c75b04
TylerMB <tyler.max.baker@gmail.com>
2017-08-28 19:15:47 +1200

    changed to column values and commented

--- SpreadsheetGrammar.swift
+++ SpreadsheetGrammar.swift
@@ -306,73 +306,87 @@ class GRCellReference : GrammarRule {
             } else if rel.stringValue != nil {
                 
 
-                enum ArrayError: Error {
-                    case OutOfBounds(min: Int, max: Int)
-                }
-                var count = 0
                 
-                var rowChars: Array = Array(GrammarRule.currentCell.row.stringValue!.characters)
-                var rowVals: Array = [Int](repeating: 0, count: rowChars.count)
+                
+                //an array of the letters in the the column label for the current cell
+                var colChars: Array = Array(GrammarRule.currentCell.col.stringValue!.characters)
+                //an empty array of numbers that the label will be converted into
+                var colVals: Array = [Int](repeating: 0, count: colChars.count)
+                //an alphabet of letters matching a number value 1-26
                 let alphabet = [" ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
                                 "R","S","T","U","V","W","X","Y","Z"]
                 
-                //var index:Int = colChars.count
-                
-                for index in stride(from: rowChars.count-1, to: -1, by: -1) {
-                    rowVals[count] = alphabet.index(of: String(rowChars[index]))!
+              
+                //reverse through the characters of the label assigning values
+                //reversed so that the last number is the highest valued letter
+                //makes it easy to add and delete the final letter
+                var count = 0
+                for index in stride(from: colChars.count-1, to: -1, by: -1) {
+                    colVals[count] = alphabet.index(of: String(colChars[index]))!
                     count += 1
                 }
                 
-                rowVals[0] += rel.row.calculatedValue!
+                //adds the r0c0 value to the lowest valued letter
+                colVals[0] += rel.col.calculatedValue!
                 
-                for index in 0...(rowVals.count-1) {
-                    while rowVals[index] > 26 {
-                        rowVals[index] -= 26
-                        if rowVals[index+1] < (rowVals.count-1) {
-                            rowVals[index+1] += 1
+                //for each of the indexs in the column values
+                for index in 0...(colVals.count-1) {
+                    //if it is greater than 26 (while loop to deal with r0c52)
+                    //27,1 -> 1,2
+                    while colVals[index] > 26 {
+                        //reduce that index value
+                        colVals[index] -= 26
+                        if (index+1) < (colVals.count-1) {
+                            //increase the value of the next letter
+                            colVals[index+1] += 1
                         } else {
-                            rowVals.append(1)
+                            //or add one if it doesnt exist
+                            colVals.append(1)
                         }
                     }
-                    while rowVals[index] < 1 {
-                        rowVals[index] += 26
-                        if rowVals[index+1] < (rowVals.count-1) {
-                            rowVals[index+1] -= 1
+                    //if letter is less than 'A', increase the value and reduce the value of the next letter
+                    while colVals[index] < 1 {
+                        colVals[index] += 26
+                        if (index+1) < (colVals.count-1) {
+                            colVals[index+1] -= 1
                         } else {
-                            rowVals[index] = 0
+                            colVals[index] = 0
                         }
                     }
                 }
-                
-                for index in 0...(rowVals.count-1) {
+                //works through the array dealing with 0 values, if the highest valued letter is 0, deletes it
+                for index in 0...(colVals.count-1) {
                     
-                    if rowVals[index] == 0 {
-                        if index == rowVals.count-1 {
-                            rowVals = Array(rowVals.dropLast(1))
+                    if colVals[index] == 0 {
+                        if index == colVals.count-1 {
+                            colVals = Array(colVals.dropLast(1))
                         } else {
-                            rowVals[index] += 26
-                            rowVals[index+1] -= 1
+                            colVals[index] += 26
+                            colVals[index+1] -= 1
                         }
                     }
-                    count = (rowVals.count-1)
-                    if index < (rowChars.count-1) {
-                        rowChars[index] = Character(alphabet[rowVals[count]])
+                    //works through the calculated values and reverses it back into colChars, with the addition done
+                    count = (colVals.count-1)
+                    if index < (colChars.count-1) {
+                        colChars[index] = Character(alphabet[colVals[count]])
                         count -= 1
                     } else {
-                        rowChars.append(Character(alphabet[rowVals[count]]))
+                        colChars.append(Character(alphabet[colVals[count]]))
                         count -= 1
                     }
                     
                 }
-                print(rowVals)
-                print(rowChars)
+                print(colVals)
+                print(colChars)
                 print(alphabet)
                 
                 var relCell = ""
-                for index in 0...(rowChars.count-1) {
-                    relCell.append(rowChars[index])
+                //builds the relativeCell final location as an absouteCell
+                for index in 0...(colChars.count-1) {
+                    relCell.append(colChars[index])
                 }
-                relCell.append(String(GrammarRule.currentCell.col.calculatedValue!+rel.col.calculatedValue!))
+                //adds the row number to the column label, with the addition of the relative row
+                relCell.append(String(GrammarRule.currentCell.row.calculatedValue!+rel.row.calculatedValue!))
                 self.stringValue = relCell
             }
             return rest
